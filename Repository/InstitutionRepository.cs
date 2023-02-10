1f6402c688128354f37814c632b1969f1d3ff7fd
José Adauto <60900449+albadauto@users.noreply.github.com>
2023-02-10 15:32:51 -0300

    Institution return all informations

--- Repository/InstitutionRepository.cs
+++ Repository/InstitutionRepository.cs
@@ -16,14 +16,23 @@ namespace api_avaliaae.Repository
             List<InstitutionModel> inst = new List<InstitutionModel>();
             var result = (from i in _context.Institution
                           join t in _context.InstitutionType on i.InstitutionTypeId equals t.Id
-                          select new { i.InstitutionName, t.Name });
+                          select new { Institution = i, t.Name, idType = t.Id});
             foreach (var i in result)
             {
                 inst.Add(new InstitutionModel()
                 {
-                    InstitutionName = i.InstitutionName,
+                    InstitutionName = i.Institution.InstitutionName,
+                    Cnpj = i.Institution.Cnpj,
+                    Phone = i.Institution.Phone,
+                    Email = i.Institution.Email,
+                    Address = i.Institution.Address, 
+                    District = i.Institution.District,
+                    Number = i.Institution.Number,  
+                    Cep = i.Institution.Cep,
+                    Description = i.Institution.Description,
                     InstitutionType = new InstitutionTypeModel()
                     {
+                        Id = i.idType,
                         Name = i.Name
                     }
                 }); ;
